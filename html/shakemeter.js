5d523a7425780fea62ab0ac6ed44de440808737f
Philip Crotwell <crotwell@seis.sc.edu>
2025-09-03 16:00:06 -0400

    update some text for debugging, wider style

--- html/shakemeter.js
+++ html/shakemeter.js
@@ -10,6 +10,8 @@ function addToDebug(message) {
   const code = pre.appendChild(document.createElement("code"));
   code.textContent = message;
 }
+const SH_HEIGHT = 300
+const SH_HALF = SH_HEIGHT
 const GARNET = "#73000a";
 const METER_MAX = 0.5;
 const METER_OFFSET = -7.1;
@@ -20,13 +22,12 @@ function drawMeter() {
   if (!meterDiv) {
     return;
   }
-  // <path d="M0,150  a150,150 0 0,1 ${150*(1+Math.cos(rad))},${150*(1-Math.sin(rad))} z"
   let rad = 179*Math.PI/180;
   let meterPathD = createArcPath(0, METER_MAX)
   meterDiv.innerHTML = `
   <svg>
-    <rect x="0" y="0" width="300" height="300" stroke="black" stroke-width="1" />
-    <circle cx="150" cy="150" r="150" fill="black"/>
+    <rect x="0" y="0" width="${2*SH_HEIGHT}" height="${SH_HEIGHT}" stroke="grey" stroke-width="1" />
+    <circle cx="${SH_HEIGHT}" cy="${SH_HEIGHT}" r="${SH_HEIGHT}" fill="black"/>
     <path d="${meterPathD}"
         fill="${GARNET}" stroke="${GARNET}" stroke-width="3" >
 
@@ -37,7 +38,7 @@ function drawMeter() {
 
 function createArcPath(val, max) {
   const rad = ((val/max*180))*Math.PI/180;
-  return `M0,150  A150,150 0 0,1 ${150*(1-Math.cos(rad))},${150*(1-Math.sin(rad))} L150,150 Z`
+  return `M0,${SH_HALF}  A${SH_HALF},${SH_HALF} 0 0,1 ${SH_HALF*(1-Math.cos(rad))},${SH_HALF*(1-Math.sin(rad))} L${SH_HALF},${SH_HALF} Z`
 }
 
 function updateMeter(val) {
@@ -51,10 +52,12 @@ function updateMeter(val) {
     d_attr = createArcPath(val, METER_MAX);
   }
   path.setAttribute("d", d_attr);
+  document.querySelector("#mag").textContent=`${val}`;
+  document.querySelector("#mag_offset").textContent=`${METER_OFFSET} range: ${METER_MAX}`;
 }
 
 let packetHandler = (daliPacket) => {
-  addToDebug(`Packet: ${daliPacket.streamId}`);
+  document.querySelector("#sid").textContent=`${daliPacket.streamId}`;
   if (daliPacket.isJson()) {
     const jdata = daliPacket.asJson();
     const st = sp.luxon.DateTime.fromISO(jdata.st);
@@ -83,7 +86,9 @@ drawMeter();
 dali.connect().then( response => {
   return dali.match(".*_M_A_G/JSON");
 }).then( response => {
-  return dali.positionAfter(sp.luxon.DateTime.utc().minus(METER_DELAY));
+  const start_dali = sp.luxon.DateTime.utc().minus(METER_DELAY);
+  addToDebug(`Start dali for mag at ${start_dali.toISO()}`);
+  return dali.positionAfter(start_dali);
 }).then( response => {
   return dali.stream();
 });
